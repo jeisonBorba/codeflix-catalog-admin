a515a9537497436453193e8b1df0040c10695f5d
jeisonBorba <jeizon.borba@gmail.com>
2023-09-15 17:48:57 -0300

    create jpa entities for video

---
+++ infrastructure/src/main/java/br/com/codeflix/catalog/admin/infrastructure/video/persistence/VideoRepository.java
@@ -0,0 +1,6 @@
+package br.com.codeflix.catalog.admin.infrastructure.video.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VideoRepository extends JpaRepository<VideoJpaEntity, String> {
+}
